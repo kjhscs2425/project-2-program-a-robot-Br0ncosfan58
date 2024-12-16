137fe1593f4e58b7e843921d886120ea32b47054
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2024-12-16 08:35:36 -0800

    draft 1

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
-# Import the robot control commands from the library
-from simulator import robot
-import time
+from simulator import robot  
+import time  
 
-left, right = robot.sonars()
-robot.motors(1, 1, 2)
- 
+left_distance, right_distance = robot.sonars()  
+robot.sonars()  
+  
+def forward(distance):  
+   robot.motors(1, 1, distance)  
+  
+def backward(distance):  
+   robot.motors(-1, -1, distance)  
+  
+def left(angle):  
+   distance = angle  
+   robot.motors(-1, 1, distance)  
+  
+def right(angle):  
+   distance = angle  
+   robot.motors(1, -1, distance)  
+  
+def draw_M():  
+   print("Drawing M...")  
+   forward(10)  
+   left(45)  
+   forward(10)  
+   right(90)  
+   left(45)  
+   backward(10)  
+   print("M drawn!") 
+  
+def draw_A():  
+   print("Drawing A...")  
+   forward(10)  
+   left(60)  
+   forward(10)  
+   right(120)  
+   forward(10)  
+   left(60)  
+   backward(10)  
+   print("A drawn!")  
+  
+def draw_X():  
+   print("Drawing X...")  
+   forward(10)  
+   left(45)  
+   forward(10)  
+   right(90)  
+   forward(10)  
+   left(45)  
+   backward(10)  
+   right(45)  
+   forward(10)  
+   left(90)  
+   forward(10)  
+   right(45)  
+   backward(10)  
+   print("X drawn!")  
+  
+draw_M()  
+draw_A()  
+draw_X()
